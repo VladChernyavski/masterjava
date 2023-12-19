d30353ea9b88b83573a529520e8e69d83b1d4c53
VladChernyavski <vlchernyavski@gmail.com>
2023-12-19 22:26:24 +0300

    fix wsdl and schema

--- services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/GroupResult.java
+++ services/mail-api/src/main/java/ru/javaops/masterjava/service/mail/GroupResult.java
@@ -1,23 +1,24 @@
 package ru.javaops.masterjava.service.mail;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import java.util.List;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
+@Getter
 public class GroupResult {
     private int success; // number of successfully sent email
     private List<MailResult> failed; // failed emails with causes
-    private String failedCause;  // global fail cause
 
     @Override
     public String toString() {
         return "Success: " + success + '\n' +
-                (failed == null ? "" : "Failed: " + failed.toString() + '\n') +
-                (failedCause == null ? "" : "Failed cause: " + failedCause);
+                (failed == null ? "" : "Failed: " + failed.toString());
     }
 }
--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -1,6 +1,7 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableSet;
+import ru.javaops.web.WebStateException;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
@@ -9,13 +10,20 @@ import java.net.URL;
 
 public class MailServiceClient {
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, WebStateException {
         Service service = Service.create(
                 new URL("http://localhost:8080/mail/mailService?wsdl"),
                 new QName("http://mail.javaops.ru/", "MailServiceImplService"));
 
         MailService mailService = service.getPort(MailService.class);
-        mailService.sendToGroup(ImmutableSet.of(
-                new Addressee("masterjava@javaops.ru", null)), null, "Subject", "Body");
+
+        String state = mailService.sendToGroup(ImmutableSet.of(new Addressee("masterjava@javaops.ru", null)), null,
+                "Group mail subject", "Group mail body");
+        System.out.println("Group mail state: " + state);
+
+        GroupResult groupResult = mailService.sendBulk(ImmutableSet.of(
+                new Addressee("Мастер Java <masterjava@javaops.ru>"),
+                new Addressee("Bad Email <bad_email.ru>")), "Bulk mail subject", "Bulk mail body");
+        System.out.println("\nBulk mail groupResult:\n" + groupResult);
     }
 }
