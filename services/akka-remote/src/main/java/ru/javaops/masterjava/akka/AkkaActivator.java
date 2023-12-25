68d4dba57a26a16e9be39f9804f7520c48fc1ac5
VladChernyavski <vlchernyavski@gmail.com>
2023-12-25 23:40:45 +0300

    akka typed

--- services/akka-remote/src/main/java/ru/javaops/masterjava/akka/AkkaActivator.java
+++ services/akka-remote/src/main/java/ru/javaops/masterjava/akka/AkkaActivator.java
@@ -1,8 +1,14 @@
 package ru.javaops.masterjava.akka;
 
-import akka.actor.ActorSystem;
+import akka.actor.*;
+import akka.japi.Creator;
+import akka.util.Timeout;
 import lombok.extern.slf4j.Slf4j;
 import ru.javaops.masterjava.config.Configs;
+import scala.concurrent.ExecutionContext;
+import scala.concurrent.duration.Duration;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class AkkaActivator {
@@ -17,6 +23,31 @@ public class AkkaActivator {
 
     public static AkkaActivator start(String actorSystemName, String configName) {
         return new AkkaActivator(actorSystemName, configName);
+    }
+
+    public <T> void startTypedActor(Class<T> typedClass, String name, Creator<T> creator) {
+        log.info("Start AKKA typed actor: {}", name);
+        TypedActor.get(system).typedActorOf(
+                new TypedProps<T>(typedClass, creator).withTimeout(new Timeout(Duration.create(20, TimeUnit.SECONDS))), name);
+    }
+
+    public <T> ActorRef startActor(Class<T> actorClass, String name) {
+        log.info("Start AKKA actor: {}", name);
+        return system.actorOf(Props.create(actorClass), name);
+    }
+
+    public <T> T getTypedRef(Class<T> typedClass, String path) {
+        log.info("Get typed reference with path={}", path);
+        return TypedActor.get(system).typedActorOf(new TypedProps<T>(typedClass), system.actorFor(path));
+    }
+
+    public ActorRef getActorRef(String path) {
+        log.info("Get actor reference with path={}", path);
+        return system.actorFor(path);
+    }
+
+    public ExecutionContext getExecutionContext() {
+        return system.dispatcher();
     }
 
     public void shutdown() {
